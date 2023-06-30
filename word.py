6377d6fa6cca3063419926579e7a0ebadda18038
Andrey Kuznetsov <kutuzkin98@mail.ru>
2023-06-30 15:59:43 +0800

    removing unnecessary docstrings

--- word.py
+++ word.py
@@ -3,27 +3,14 @@ class BasicWord:
         self.word = word
         self.subwords = subwords
 
-
     def __repr__(self):
         return f'''BasicWord({self.word}, {len(self.subwords)})'''
 
-
-    def has_subword(self, attempt):
-        """
-        Проверка введённого слова в списке подпустимых подслов
-        """
+    def has_subword(self, attempt): # проверка введённого слова в списке подпустимых подслов
         return attempt.strip().lower() in self.subwords
 
-
     def counting_subwords(self):
-        """
-        Подсчёт количества подслов
-        """
         return len(self.subwords)
 
-
     def get_word(self):
-        """
-        Возвращает полное слово
-        """
         return self.word
