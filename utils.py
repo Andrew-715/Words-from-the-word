7d6b59e171ccde75041137141c78d85c603f2dab
Andrey Kuznetsov <kutuzkin98@mail.ru>
2023-06-30 15:58:28 +0800

    removing unnecessary docstrings

--- utils.py
+++ utils.py
@@ -3,21 +3,13 @@ import requests
 from word import BasicWord
 
 
-def load_json_from_jk(path):
-    """
-    Загружает данные по ссылке и возвращает их в виде списка или словаря
-    :param path: путь к источнику данных
-    :return:
-    """
+def load_json_from_jk(path): # загружает данные по ссылке и возвращает их в виде списка или словаря
     response = requests.get(path)
     response_json = response.json()
     return response_json
 
 
-def load_random_word(path):
-    """
-    Генерирует случайные слова и подслова, а также вызывает класс BasicWord
-    """
+def load_random_word(path): # генерирует случайные слова и подслова
     all_words = load_json_from_jk(path)
     random_word = random.choice(all_words)
     word = random_word['word']
